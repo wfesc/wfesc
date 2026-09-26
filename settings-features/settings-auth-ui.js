76bbe1322058099b25f3950ba929259a5590e186
wfesc <sonwlord90@gmail.com>
2026-09-26 19:39:53 +0300

    Remove init function from settings-auth-ui.js
    Removed the init function and its implementation.

--- settings-features/settings-auth-ui.js
+++ settings-features/settings-auth-ui.js
@@ -1,24 +1,4 @@
-function init() {
 
-    alert("WFESC UI اشتغل");
-
-    ensureRoot();
-
-    buildUI();
-
-    injectCSS();
-
-    bindEvents();
-
-    bindAuthEvents();
-
-    handleRecoveryURL();
-
-    setMode("login");
-
-    restoreSession();
-
-}
 /*
  * WFESC Settings Auth UI
  * Complete UI controller for settings.html
